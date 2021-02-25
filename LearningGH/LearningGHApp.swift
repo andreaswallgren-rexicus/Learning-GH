2951f73b5325dd17fa21aebd3c14d925f09bd870
Andreas Wallgren <andreas.wallgren@awab.se>
2021-02-25 19:20:46 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Andreas Wallgren on 2021-02-25.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
